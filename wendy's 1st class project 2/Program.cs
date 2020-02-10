8993d5e80eaac56b4887721a33309cc8d9ee880e
Wendy Figueroa <wendy.e.figueroa@outlook.com>
2020-02-10 00:49:50 -0500

    Add project files.

---
+++ wendy's 1st class project 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace wendy_s_1st_class_project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
